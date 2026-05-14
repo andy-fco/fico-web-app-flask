0686abf783f7d79cbf37577c440ef266c9fd92a5
Andrés Fernandez <andyfernandez2002@gmail.com>
2026-05-14 16:30:53 -0300

    Template structure - Public templates, components HTML & CSS

--- app.py
+++ app.py
@@ -4,7 +4,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def inicio():
-    return render_template('index.html')
+    return render_template('public/index.html')
+
+@app.route('/login')
+def login():
+    return render_template('auth/login.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('auth/register.html')
+
+@app.route('/simulador')
+def simulador():
+    return render_template('public/simulador.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
